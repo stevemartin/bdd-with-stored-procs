e754aac357b6fe09ebb9626d05691d56b8117a19
Steve Martin <steve.martin@fundingcircle.com>
2013-08-06 00:55:30 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoredProcTest::Application.config.secret_key_base = '87383d279799fd5dee41ee68cd2b74de27c55b10fed257a3873b666430918d2c2704df1d2f96e994376837ce734185804cf1c29c647f1aa41a87e1605b896722'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StoredProcTest::Application.config.session_store :cookie_store, key: '_stored_proc_test_session'
