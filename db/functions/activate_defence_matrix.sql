acba7a50d954a3ecd5b1fd99c5c533e0153815b9
Steve Martin <steve.martin@fundingcircle.com>
2013-08-08 22:27:06 +0100

    prepare to activate the defence matrix

---
+++ db/functions/activate_defence_matrix.sql
@@ -0,0 +1,12 @@
+  SET search_path = hq, pg_catalog;
+  --
+  -- Name: activate_defence_matrix(); Type: FUNCTION; Schema: hq; Owner: steve
+  --
+
+  CREATE OR REPLACE FUNCTION hq.activate_defence_matrix() RETURNS void
+  LANGUAGE plpgsql SECURITY DEFINER
+  AS $$
+    DECLARE
+    BEGIN
+    END
+  $$;
---
+++ spec/stored_procs/activate_defence_matrix_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe 'the user report stored procedure' do
+  before(:all) do
+    @defence_matrix = File.read('db/functions/activate_defence_matrix.sql')
+    @res = ActiveRecord::Base.connection.execute @defence_matrix
+  end
+
+  it 'should rebuild the function' do
+    @res.should be_a PG::Result
+  end
+
+  it 'should exist' do
+    stored_proc_exists?('activate_defence_matrix').should == true
+  end
+
+
+  it 'should contain the stored proc' do
+    stored_proc_src("activate_defence_matrix").should == %q|
+    DECLARE
+    BEGIN
+    END
+  |
+  end
+end
+
+def arex sql
+  ActiveRecord::Base.connection.execute sql
+end
+
+def stored_proc_exists? name
+  res = arex("SELECT proname FROM pg_catalog.pg_proc WHERE proname = '#{name}'")
+  res = res.first
+  if res
+    return res['proname'] == name
+  else
+    false
+  end
+end
+
+
+def stored_proc_src name
+  res = arex("select prosrc from pg_catalog.pg_proc where proname = '#{name}';")
+  res = res.first
+  if res
+    return res['prosrc']
+  else
+    false
+  end
+
+end
